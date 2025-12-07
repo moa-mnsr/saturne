cfd804b1988c968cb98dee89d4a733b3c40055e5
M Mansour <64868740+moa-mnsr@users.noreply.github.com>
2025-12-07 01:36:33 +0100

    tightening JS order & redundancies

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
+// set channel on load
+let channelNumber = 0;
+// say what the background is that you want to change later
+const panels = document.querySelectorAll('.typed-panel svg');
+// say what the buttons are
+const buttons = document.querySelectorAll('button');
+
 // make sure all panels hide on load
-document.querySelectorAll('.typed-panel svg').forEach(panel => {
+panels.forEach(panel => {
     panel.style.display ='none';
 });
 
-// say what the background is that you want to change later
-const panels = document.querySelectorAll('.typed-panel svg');
-
 // set buttons to listen for click
-document.querySelectorAll('button').forEach((button, index) => {
+buttons.forEach((button, index) => {
   button.addEventListener('click', function() {
     changeChannel(index+1);
 });
 });
 
-// set channel initially
-let channelNumber = 0;
-
 // say what it means to change channels
 function changeChannel(buttonNumber) {
-  // set the divs to hide initialy
+  // set the divs to hide at first
   panels.forEach(panel => {
   panel.style.display = 'none';
 });
