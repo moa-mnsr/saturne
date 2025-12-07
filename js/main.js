f9616efb801e79e5779b8c27281e6bf55a374262
M Mansour <64868740+moa-mnsr@users.noreply.github.com>
2025-12-07 02:01:02 +0100

    moving visibility to css from js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
-// set channel on load
+// make sure the channel is zero and text on the panel is not visible on load
 let channelNumber = 0;
-// say what the background is that you want to change later
+// say where to find the panels in the html so that that you can change their visibility later
 const panels = document.querySelectorAll('.typed-panel svg');
-// say what the buttons are
+// find the buttons so you can call on them later
 const buttons = document.querySelectorAll('button');
 
-// make sure all panels hide on load
-panels.forEach(panel => {
-    panel.style.display ='none';
-});
-
-// set buttons to listen for click
+// set buttons to listen for clicks to each
 buttons.forEach((button, index) => {
   button.addEventListener('click', function() {
     changeChannel(index+1);
 });
 });
 
-// say what it means to change channels
+// say what it means to change channels and how it relates to button numbers
 function changeChannel(buttonNumber) {
-  // set the divs to hide at first
+  // make sure only one panel is visible at a time by turning the display off just in case
   panels.forEach(panel => {
   panel.style.display = 'none';
 });
 
   // logic of the channel switching
+  // if channel is already set to the button's number
+  // then it's on and needs to be switched off
   if (channelNumber === buttonNumber) {
     channelNumber = 0;
+  // if it is any other channel or zero, it can freely change to the match the switch
   } else {
     channelNumber = buttonNumber;
   }
     
-console.log('channelNumber is:', channelNumber);
+// this is just here for debugging
+  console.log('channelNumber is:', channelNumber);
 
+// if the channel is showing something, program can reverse the index+1 set in changeChannel
+// and count the panels with the channelNumber minus one, and set those to show
 if (channelNumber !==0) {
   panels[channelNumber - 1].style.display = 'block';
 }
