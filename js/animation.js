c0ab70b46ff8a5d504f4d1b590584c6816f4810b
M Mansour <64868740+moa-mnsr@users.noreply.github.com>
2026-02-04 10:26:34 +0100

    animation starting to work..

--- js/animation.js
+++ js/animation.js
@@ -26,20 +26,33 @@ handwritingLayerDiv.onscroll = (event) => {
 // First animation (top) .. this is just to test where it is
 const entangledPosition = entangled.getBoundingClientRect();
 console.log('entangled top:', entangledPosition.top);
+console.log('entangled left:', entangledPosition.left);
 
 // ..coupled with the bottom one that will eventually triggers its release
 const wastePosition = waste.getBoundingClientRect();
-console.log('waste bottom:', wastePosition.bottom);
+console.log('waste top:', wastePosition.top);
+console.log('waste right:', wastePosition.right);
 
 // now that we can log where it is at all, we can decide on a point at which we want it to catch on the screen and get stuck there for a bit
 
-if (entangledPosition.top <= -62) {
+if (entangledPosition.top <= -62 && entangled.style.position !== 'sticky') {
   console.log('entangled is caught!');
+  entangled.style.position = 'sticky';
+  entangled.style.top = '-62px';
+  entangled.style.left = '-42px';
+  entangled.style.zIndex = '5';
+  entangled.style.transform = 'none';
+
 }
 
-if (wastePosition.bottom <= 1582.5) {
-  console.log('waste is caught!');
-}
+//if (wastePosition.top <= 884) {
+//    console.log('waste is caught!');
+//   waste.style.position = 'sticky';
+//   waste.style.top = '884px';
+//  waste.style.right = '1192px';
+//  waste.style.zIndex = '5';
+//  waste.style.transform = 'translate(10vh, -52vh)';
+//}
   // commenting out log so that console ist flooded for now
 
   // Check if a threshold was passed.
